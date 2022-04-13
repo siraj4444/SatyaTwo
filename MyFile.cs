3c487ca0b53b535170eab2616697d38ca045b16a
siraj <siraj.13207@gmail.com>
2022-04-13 20:12:15 +0530

    Add new file called MyFile.cs

---
+++ MyFile.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace EntityDbFirst
+{
+    public partial class TblProducts
+    {
+        public int ProductId { get; set; }
+        public string Name { get; set; }
+        public decimal? Price { get; set; }
+        public int? CategoryId { get; set; }
+
+        public virtual TblCategories Category { get; set; }
+    }
+}
