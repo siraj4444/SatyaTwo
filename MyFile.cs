f256e7a2041a07c8880056380d9b1efd9f477eec
siraj <siraj.13207@gmail.com>
2022-04-14 06:43:32 +0530

    Local Dev One REMOVED ProductId Property

--- MyFile.cs
+++ MyFile.cs
@@ -5,7 +5,7 @@ namespace EntityDbFirst
 {
     public partial class TblProducts
     {
-        public int ProductId { get; set; }
+        
         public string Name { get; set; }
         public decimal? Price { get; set; }
         public int? CategoryId { get; set; }
